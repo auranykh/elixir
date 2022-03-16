4425a6cd1b55eb92aea5920589066c17e28a7bb8
ada <10267293+auranykh@users.noreply.github.com>
2022-03-15 23:41:27 -0400

    Formatted kitchen calculator exercism

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -5,6 +5,7 @@ defmodule KitchenCalculator do
 
   def to_milliliter(volume_pair) when is_tuple(volume_pair) and tuple_size(volume_pair) == 2 do
     {type, _} = volume_pair
+
     case type do
       :cup -> {:milliliter, get_volume(volume_pair) * 240}
       :fluid_ounce -> {:milliliter, get_volume(volume_pair) * 30}
@@ -15,7 +16,8 @@ defmodule KitchenCalculator do
     end
   end
 
-  def from_milliliter(volume_pair, unit) when is_tuple(volume_pair) and tuple_size(volume_pair) == 2 and is_atom(unit) do
+  def from_milliliter(volume_pair, unit)
+      when is_tuple(volume_pair) and tuple_size(volume_pair) == 2 and is_atom(unit) do
     case unit do
       :cup -> {unit, get_volume(volume_pair) / 240}
       :fluid_ounce -> {unit, get_volume(volume_pair) / 30}
@@ -26,18 +28,23 @@ defmodule KitchenCalculator do
     end
   end
 
-  def convert(volume_pair, unit) when is_tuple(volume_pair) and tuple_size(volume_pair) == 2 and is_atom(unit) do
+  def convert(volume_pair, unit)
+      when is_tuple(volume_pair) and tuple_size(volume_pair) == 2 and is_atom(unit) do
     base_unit = elem(volume_pair, 0)
+
     case {base_unit, unit} do
       # from milliliter to milliliter
       {:milliliter, :milliliter} ->
         volume_pair
+
       # from milliliter to x
       {:milliliter, _} ->
         from_milliliter(volume_pair, unit)
+
       # from x to milliliter
       {_, :milliliter} ->
         to_milliliter(volume_pair)
+
       # from x to y
       {_, _} ->
         from_milliliter(to_milliliter(volume_pair), unit)
