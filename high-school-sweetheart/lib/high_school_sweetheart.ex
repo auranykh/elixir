0baaa73718fa1e879eae522eea83ed116557dcb9
ada <10267293+auranykh@users.noreply.github.com>
2022-03-16 22:36:22 -0400

    Completed high school sweetheart exercism

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,54 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name)
+    |> String.at(0)
+  end
+
+  def initial(name) do
+    first_letter(name)
+    |> String.capitalize()
+    |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    # not strongly enforced, edge case issues with a single name being passed in.
+    [first_name, last_name] = String.split(full_name)
+    ini1 = initial(first_name)
+    ini2 = initial(last_name)
+    ini1 <> " " <> ini2
+  end
+
+  def pair(full_name1, full_name2) do
+    #      ******       ******
+    #    **      **   **      **
+    #  **         ** **         **
+    # **            *            **
+    # **                         **
+    # **     X. X.  +  X. X.     **
+    #  **                       **
+    #    **                   **
+    #      **               **
+    #        **           **
+    #          **       **
+    #            **   **
+    #              ***
+    #               *
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
