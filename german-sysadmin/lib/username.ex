9ca224d7833b7a050d0d299b802315c37f09a9b1
ada <10267293+auranykh@users.noreply.github.com>
2022-03-22 21:21:40 -0400

    Started german sysadmin exercism

---
+++ german-sysadmin/lib/username.ex
@@ -0,0 +1,14 @@
+defmodule Username do
+  def sanitize(username) do
+    # ä becomes ae
+    # ö becomes oe
+    # ü becomes ue
+    # ß becomes ss
+
+    # Please implement the sanitize/1 function
+  end
+
+  defp remove_nums(username) do
+
+  end
+end
