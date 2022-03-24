57675e9ad054fb129adf8d54d1866ed3909c895a
ada <10267293+auranykh@users.noreply.github.com>
2022-03-23 23:31:26 -0400

    Continuing work on german sysadmin exercism

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,14 +1,23 @@
 defmodule Username do
   def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+    inner_sanitize(username)
   end
 
-  defp remove_nums(username) do
+  defp inner_sanitize('') do
+    ''
+  end
 
+  defp inner_sanitize([head | tail]) do
+    sanitized = case head do
+      ?ß -> 'ss'
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      x when x >= ?a and x <= ?z -> [x]
+      ?_ -> '_'
+      _ -> ''
+    end
+
+    sanitized ++ sanitized(tail)
   end
 end
